c974f997ba857f91793081ef9ab0f4d38675b168
amirabid123 <138674820+amirabid123@users.noreply.github.com>
2023-12-08 21:47:22 +0100

    Delete TSYP11-Technical-Challenge-amir directory

--- TSYP11-Technical-Challenge-amir/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Farmer = artifacts.require("./Farmer.sol");
-var suply = artifacts.require("./Supplychain.sol");
-module.exports = function(deployer) {
-  deployer.deploy(suply);
-};
--- TSYP11-Technical-Challenge-amir/src/componentsInspector/InspectorTable/DataTableI/datatablesource.js
+++
@@ -1,9 +0,0 @@
-export const userColumns=[
-    { field: 'id', headerName: 'ID', width: 50 },
-    { field: 'manufacturer_name', headerName: 'Full Name of the Manufacturer ', width: 230},
-
-    { field: "Pdate", headerName: "Production Date", width: 150,},
-    { field: "quantity", headerName: "Quantity (L)", width: 120 },
-    { field: "method", headerName: "Production Method", width: 150,},
-    { field: "temprature", headerName: "Smoke Point Temprature", width: 200,}
-]
